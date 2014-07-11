63ed16fc9d39e2d08e5e0b02e8251e7ccc2feffe
sharmads <dpsharma101@gmail.com>
2014-07-11 10:51:13 -0400

    R Code to plot 4 graphs -Assignment1
    R Code to plot 4 graphs - Exploratory Data Analysis Course Project 1.
    Please review all 4 code sets to plot the graphs.. Also you can run the codeset to see and plot the graphs..

---
+++ Exp_DataAnalysis_CourseProject1.R
@@ -0,0 +1,64 @@
+# Exploratory Data Analysis -- Course Project 1
+# Dharmpal Sharma on 11-JUL-2014
+#
+
+options(stringsAsFactors = FALSE)
+options(scipen=100) 
+options(width=100)
+
+kRoot <- "C:/RDev/Data/Expl_Analysis/"
+epc <- read.table(paste(kRoot,"household_power_consumption.txt",sep=""),sep=";",header=TRUE)
+epc$Date <- as.Date(epc$Date, format="%d/%m/%Y")
+epc.subset <- subset(epc, Date >= "2007-02-01" & Date <= "2007-02-02")
+epc.subset$Time <- strptime(epc.subset$Time,format="%H:%M:%S")
+
+# Plot 1
+epc.subset$Global_active_power <- as.numeric(epc.subset$Global_active_power)
+png(filename=paste(kRoot,"plot1.png"),width=480,height=480)
+hist(epc.subset$Global_active_power, col="orangered",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
+
+# Plot 2
+epc.subset$DOW <- format(epc.subset$Date, "%a")
+epc.subset$DOW <- as.factor(epc.subset$DOW)
+with(epc.subset[,c("Global_active_power","DOW")], {
+    png(filename=paste(kRoot,"plot2.png"),width=480,height=480)
+    plot(Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xaxt="n",xlab="")
+    axis(1,c(1,1441,2880),labels=c("Thu","Fri","Sat"))
+    dev.off()
+  }
+)
+
+
+# Plot 3
+with(epc.subset, {
+  png(filename=paste(kRoot,"plot3.png"),width=480,height=480)
+  plot(Sub_metering_1,type="l",ylab="Energy sub metering",xaxt="n",xlab="")
+  lines(Sub_metering_2,type="l",col="red")
+  lines(Sub_metering_3,type="l",col="blue")
+  axis(1,c(1,1441,2880),labels=c("Thu","Fri","Sat"))
+  legend(x="topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+         col=c("black","red","blue"),lty=1)
+  dev.off()
+}
+)
+
+# Plot 4
+with(epc.subset, {
+  png(filename=paste(kRoot,"plot4.png"),width=480,height=480)
+  par(mfrow=c(2,2))
+  plot(Global_active_power,type="l",ylab="Global Active Power",xaxt="n",xlab="")
+  axis(1,c(1,1441,2880),labels=c("Thu","Fri","Sat"))
+  plot(Voltage,type="l",xaxt="n",xlab="datetime")
+  axis(1,c(1,1441,2880),labels=c("Thu","Fri","Sat"))
+  plot(Sub_metering_1,type="l",ylab="Energy sub metering",xaxt="n",xlab="")
+  lines(Sub_metering_2,type="l",col="red")
+  lines(Sub_metering_3,type="l",col="blue")
+  axis(1,c(1,1441,2880),labels=c("Thu","Fri","Sat"))
+  legend(x="topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+         col=c("black","red","blue"),lty=1,border=FALSE)
+  plot(Global_reactive_power,type="l",ylab="Energy sub metering",xaxt="n",xlab="datetime")
+  axis(1,c(1,1441,2880),labels=c("Thu","Fri","Sat"))
+  dev.off()
+}
+)
