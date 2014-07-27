1653b3b2d78d4934e68da0d4a5dcb4a9df7bcbd2
sharmads <dpsharma101@gmail.com>
2014-07-27 14:40:56 -0400

    Project 2 assignment files

---
+++ proj2_Plot1.R
@@ -0,0 +1,12 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+NEIsample <- NEI[sample(nrow(NEI), size=1000, replace=F), ]
+
+Emissions <- aggregate(NEI[, 'Emissions'], by=list(NEI$year), FUN=sum)
+Emissions$PM <- round(Emissions[,2]/1000,2)
+
+png(filename='plot1.png')
+barplot(Emissions$PM, names.arg=Emissions$Group.1, 
+        main=expression('Total Emission of PM'[2.5]), xlab='Year', ylab=expression(paste('PM', ''[2.5], ' in Kilotons')))
+dev.off()
